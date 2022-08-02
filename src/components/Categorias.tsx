20cf36d7628d8542c64d343b2e18b86d025b4027
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-08-02 15:37:52 -0300

    desenvolvendo...

--- src/components/Categorias.tsx
+++ src/components/Categorias.tsx
@@ -1,7 +1,24 @@
+import { Code, Desktop, DeviceMobile } from "phosphor-react";
+
+
 export default function Categorias() {
     return(
-        <div>
-            <h1>Categorias</h1>
+        <div className="mt-20 ">
+            <ul className="text-white flex items-center justify-center gap-10">
+                <li className="w-[15vw] h-[15vw] bg-gray-700 rounded-xl flex flex-col justify-center items-center transform motion-safe:hover:scale-110 ... transition-all shadow-md">
+                    <Desktop size={50} color="#FFFF00" weight="light" />
+                    <strong>Vazio</strong>
+                </li>
+                <li className="w-[15vw] h-[15vw] bg-gray-700 rounded-xl flex flex-col justify-center items-center transform motion-safe:hover:scale-110 ... transition-all shadow-md">
+                    <Code size={50} color="#FFFF00" weight="fill" />
+                    <strong>Vazio</strong>
+                </li>
+                <li className="w-[15vw] h-[15vw] bg-gray-700 rounded-xl flex flex-col justify-center items-center transform motion-safe:hover:scale-110 ... transition-all shadow-md">
+                    <DeviceMobile size={50} color="#FFFF00" weight="thin" />
+                    <strong>Vazio</strong>
+                </li>
+               
+            </ul>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,20 @@
+
+
+
 export default function Header() {
+    let titulo = 'Meus projetos';
+   
+        const array = titulo.split('')
+        array.forEach((letra, i) => {
+            setTimeout(() => {
+                titulo += letra
+            }, 75 * i)
+        })
+
     return(
         <div className="text-center flex flex-col mt-[10vw]">
-            <strong className="text-6xl text-white">Meus projetos</strong>
+            <strong className="text-6xl text-white"> {titulo}
+            </strong>
             <div className="mt-5 mb-3 text-yellow-300">
                 <strong className="text-xs">Alguns projetos desenvolvidos por mim</strong>
             </div>
