2bcdb4edf0a89dcd85c175ddb5c5595edf425f58
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-08-02 10:01:09 -0300

    criando projeto

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Categorias from "./components/Categorias";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return(
+    <div>
+        <Navbar />
+        <Header />
+        <Categorias />
+        <Footer />
+    </div>
+  )
+
+}
+
+
