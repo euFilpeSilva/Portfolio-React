c7931fc9c0c874edf844577cb029589f5465ef4f
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-08-02 11:30:06 -0300

    desenvolvendo Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return(
-        <div>
-            <h1>Header</h1>
+        <div className="text-center flex flex-col mt-[10vw]">
+            <strong className="text-6xl text-white">Meus projetos</strong>
+            <div className="mt-5 mb-3 text-yellow-300">
+                <strong className="text-xs">Alguns projetos desenvolvidos por mim</strong>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 export default function Navbar() {
     return(
-        <div className="text-white w-full flex justify-between my-[2vw]">
-            <strong className="ml-10">EuFilpeSilva</strong>
-            <div className="flex">
-                <button className="text-yellow-300" >Curriculo</button> 
-                <button className="mr-10 ml-3 bg-gray-700 p-2 rounded-full w-[7vw] flex gap-3 items-center">
+        <div className="text-white w-full flex justify-between my-[3vw] pl-20 pr-20">
+            <strong className="ml-10 flex text-2xl"> <div className="text-yellow-300">Eu</div>FilpeSilva</strong>
+            <div className="flex mr-3">
+                <button className="text-yellow-300 text-lg ml-2 mr-4" >Curriculo</button> 
+                <button className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full w-[8vw] flex overflow-hidden">
                     <div className="ml-2"> GitHub</div> 
-                    <img src="https://github.com/euFilpeSilva/Portfolio-React/blob/main/src/assets/logo_github.svg" />
+                    <div className=" w-10 ml-2">
+                        <img src="https://raw.githubusercontent.com/euFilpeSilva/Portfolio-React/b448163e51261b964d7f61e05988158110166dee/src/assets/logo_github.svg" />
+                    </div>
                 </button>
             </div>
         </div>
